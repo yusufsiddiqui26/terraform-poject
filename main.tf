5b9d1c694657a91af7380b2a5cb374d545d98086
Yusuf Siddiqui <yusuf123siddiqui@gmail.com>
2021-05-21 15:35:26 +0530

    used vpc modules

--- main.tf
+++ main.tf
@@ -1,29 +1,31 @@
 
-resource "aws_vpc" "myapp-vpc" {
-    cidr_block = var.vpc_cidr_block
-    tags = {
-      Name = "${var.env_prefix}-vpc"
-    }
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-module "myapp-subnet" {
-  source = ".\\modules\\subnet"
-  subnet_cidr_block = var.subnet_cidr_block
-  avail_zone = var.avail_zone
-  env_prefix = var.env_prefix
-  vpc_id = aws_vpc.myapp-vpc.id
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  name = "my-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs             = [var.avail_zone]
+  public_subnets  = [var.subnet_cidr_block]
+  public_subnet_tags = {Name = "${var.env_prefix}-subnet-1"}
+
+
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 module "myapp-server" {
   source = ".\\modules\\webserver"
   image_name = var.image_name
-  vpc_id = aws_vpc.myapp-vpc.id
+  vpc_id = module.vpc.vpc_id
   my_ip = var.my_ip
   env_prefix = var.env_prefix
   public_kay_location = var.public_kay_location
   instance_type = var.instance_type
-  subnet_id = module.myapp-subnet.subnet.id
+  subnet_id = module.vpc.public_subnets[0]
   docker_script = var.docker_script
   avail_zone = var.avail_zone
 }
